e8de9d9e1e7bf405e983d72490fc15b01c4e6461
Johan Sundqvist <sundqvist.johan@outlook.com>
2020-01-10 11:31:22 +0100

    Updates API url for deployment

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import axios from "axios";
 
-axios.defaults.baseURL = "http://localhost:3000/api/v1"
+axios.defaults.baseURL = "https://jsundq-cooper-api.herokuapp.com/api/v1"
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
