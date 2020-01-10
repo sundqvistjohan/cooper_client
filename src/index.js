6ba46a1a291ed13faeb834e2fc27b0217037220d
Johan Sundqvist <sundqvist.johan@outlook.com>
2020-01-10 11:56:50 +0100

    Adds environment variables for API url

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import axios from "axios";
 
-axios.defaults.baseURL = "https://jsundq-cooper-api.herokuapp.com/api/v1"
+axios.defaults.baseURL = process.env.REACT_APP_BASEURL
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
